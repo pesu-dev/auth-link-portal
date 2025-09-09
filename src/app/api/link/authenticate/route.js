938abef559c06635102d5d90c5083ec8d53167f5
achyu-dev <achyugeorge7744@gmail.com>
2025-09-09 20:22:02 +0530

    added student check

--- src/app/api/link/authenticate/route.js
+++ src/app/api/link/authenticate/route.js
@@ -92,6 +92,29 @@ export async function GET(request) {
 
     // Check if the PESU profile contains all the required fields
     const pesuUserProfile = response.data.profile;
+
+    // Check if its a student
+    if (!isValidPrnOrSrn(pesuUserProfile.prn)) {
+      await sendErrorLogsToDiscord({
+        content: `<@${discordUser.id}>`,
+        embed: {
+          title: "Non-PRN/SRN Link Attempt Detected",
+          color: 0xffa500,
+          timestamp: new Date(),
+          footer: { text: "PESU Bot" },
+          fields: [
+            { name: "Attempted Username", value: username },
+            { name: "Returned PRN", value: pesuUserProfile.prn || "N/A" },
+            {
+              name: "Discord User",
+              value: `${discordUser.username} (${discordUser.id})`,
+            },
+            { name: "Profile", value: JSON.stringify(pesuUserProfile) },
+          ],
+        },
+      });
+    }
+
     for (const field of payload.fields) {
       if (!pesuUserProfile[field]) {
         console.error(
