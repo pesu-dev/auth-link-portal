61985122ad4b8ac8b492f8bfe65a7e1377285783
achyu-dev <achyugeorge7744@gmail.com>
2026-05-24 10:12:14 +0530

    added b.des, removed duplicate bdes role

--- src/utils/config.js
+++ src/utils/config.js
@@ -25,7 +25,6 @@ export const CONSTANTS = {
         "B.Com": "984846642354192484",
         "BBA (Hons) in Business Analytics": "1023509952964341820",
         "B.Com (Hons) with ACCA": "1023510367026036826",
-        "B.Des": "984846630396235850",
         "Psychology": "1023510685705044009",
         "Sports Management": "1023511154649223240",
         "Bachelor of Pharmacy": "1023512100724817940",
@@ -90,6 +89,6 @@ export const CONSTANTS = {
     "-Bachelor of Business Administration": "BBA",
     "Master of Business Administration": "MBA",
     "Master of Computer Applications": "MCA",
-    "Bachelor of Design": "B.Des",
+    "Bachelor of Design": "B.DES",
   },
 };
